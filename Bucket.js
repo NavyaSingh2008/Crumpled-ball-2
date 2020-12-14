3b1213331fbad83c8a8d261b42b467a615a6f5f7
NavyaSingh2008 <72865209+NavyaSingh2008@users.noreply.github.com>
2020-12-14 14:00:30 +0530

    Add files via upload

---
+++ Bucket.js
@@ -0,0 +1,19 @@
+class Bucket{
+    constructor(x,y,width,height){
+    var options={
+        isStatic:true
+    }
+    this.body = Bodies.rectangle(x,y,width,height,options);
+    this.width = width;
+    this.height = height;
+    World.add(world,this.body);
+    this.image = loadImage("dustbingreen.png");
+    }
+    display(){
+        var pos = this.body.position;
+    imageMode(CENTER);
+    fill("red");
+    image(this.image,1000,495,200,200);
+    }
+    
+    }
